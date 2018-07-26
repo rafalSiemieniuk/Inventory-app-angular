b8943b5df8d6a4416ccdb860fd03c1e9fd115314
malawrednaasia <asik.aaaaa@gmail.com>
2018-07-26 16:23:34 +0200

    #6 - onsenui added, #7 menu - first tempalte without content

---
+++ src/app/menu.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Observable, Subject} from "rxjs/index";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+
+  constructor() { }
+  subject = new Subject();
+  get menu$(): Observable<any>{
+    return this.subject.asObservable();
+  }
+  open() {
+    this.subject.next();
+  }
+}
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {SideMenuComponent} from '../side-menu/side-menu.component';
+import {ContentMenuComponent} from '../content-menu/content-menu.component';
+import {MenuService} from '../menu.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  sideMenu = SideMenuComponent;
+  contentMenu = ContentMenuComponent;
+  @ViewChild('splitter')splitter;
+  constructor (private menuService: MenuService) {
+    this.menuService.menu$.subscribe(() => this.splitter.nativeElement.side.open());
+  }
+
+
+  ngOnInit() {
+  }
+
+}
