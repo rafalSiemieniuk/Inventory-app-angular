a6868946f7e4ba4d71db32bd0ff304647e182b29
Misiek40k <comanderrr@gmail.com>
2018-07-30 15:38:47 +0200

    #3 master merged tweaks

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -5,9 +5,10 @@ import { ProfileComponent } from './profile.component';
 import { ProfileRouting } from './profile.routing';
 import { ProfileEditComponent } from './profile-edit.component';
 import { ProfileMyComponent } from './profile-my/profile-my.component';
+import { AllDevicesComponent } from './all-devices/all-devices.component';
 
 @NgModule({
-    declarations: [ProfileComponent, ProfileEditComponent, ProfileMyComponent],
+    declarations: [ProfileComponent, ProfileEditComponent, ProfileMyComponent, AllDevicesComponent],
     imports: [CommonModule, ProfileRouting],
     exports: [ProfileRouting],
     providers: [],
