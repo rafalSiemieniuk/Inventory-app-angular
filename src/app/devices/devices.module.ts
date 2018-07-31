4b3081b761210b6a0a4ee0920d0aa065ac224848
Unknown <n3ropl@gmail.com>
2018-07-31 22:07:15 +0200

    #5 added: empty component QrcodeReader

--- src/app/devices/devices.module.ts
+++ src/app/devices/devices.module.ts
@@ -5,11 +5,12 @@ import { DevicesComponent } from './devices.component';
 import { DevicesRouting } from './devices.routing';
 import { AllDevicesComponent } from './all-devices/all-devices.component';
 import { MyDevicesComponent } from './my-devices/my-devices.component';
+import { QrcodeReaderComponent } from '../qrcode-reader/qrcode-reader.component';
 
 
 
 @NgModule({
-    declarations: [DevicesComponent, AllDevicesComponent, MyDevicesComponent],
+    declarations: [DevicesComponent, AllDevicesComponent, MyDevicesComponent, QrcodeReaderComponent],
     imports: [CommonModule, DevicesRouting],
     exports: [DevicesRouting],
     providers: [],
