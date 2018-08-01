a7c41b1bc3c745f4a97f49d01a3c91a8e1edfe8f
Misiek40k <comanderrr@gmail.com>
2018-08-01 15:09:36 +0200

    #15 profile-edit @viewChild

--- src/app/profile/profile-edit/profile-edit.component.ts
+++ src/app/profile/profile-edit/profile-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, ViewChildren } from '@angular/core';
 import { ProfileService } from '../profile.service';
 import { AuthService } from '../../core/auth.service';
 
@@ -15,36 +15,34 @@ export class ProfileEditComponent implements OnInit {
   offices: any[] = [];
   data = <any>{};
 
+
+  @ViewChild('nameEdit') nameEdit: ElementRef;
+  @ViewChild('officeEdit') officeEdit: ElementRef;
+
   constructor(private profileService: ProfileService, private authService: AuthService) {
 
   }
 
 
   ngOnInit() {
-    this.profileService.getOffices().subscribe((item) => {
-      item.forEach(e => {
-        this.offices.push(e);
-      });
+    this.profileService.getOffices().subscribe((items) => {
+      this.offices = items;
     });
 
 
   }
   onSubmit() {
     this.data = this.authService.user;
-    this.data.name = (document.getElementById('name') as HTMLInputElement).value;
+    this.data.name = this.nameEdit.nativeElement.value;
 
-    const filterOffices = this.offices.filter(function (office) {
-      return office.name === (document.getElementById('office') as HTMLSelectElement).value;
+    const filterOffices = this.offices.filter((office) => {
+      return office.name === this.officeEdit.nativeElement.value;
     });
 
     const editedOffice = filterOffices[0];
 
-
-    console.log(editedOffice);
-
     this.data.officeId = editedOffice.id;
 
-    console.log(this.data);
 
   }
 }
