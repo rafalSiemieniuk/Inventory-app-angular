52f37828170310f533e22c9701f04b736874c51f
Misiek40k <comanderrr@gmail.com>
2018-08-07 12:17:43 +0200

    #18 Device name added

--- src/app/devices/my-devices-details/my-devices-details.component.ts
+++ src/app/devices/my-devices-details/my-devices-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DevicesService } from '../devices.service';
 
 
 @Component({
@@ -8,14 +9,18 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./my-devices-details.component.scss']
 })
 export class MyDevicesDetailsComponent implements OnInit {
-  device: any;
-  constructor(private route: ActivatedRoute) { }
+  device = null;
+  constructor(private route: ActivatedRoute, private devicesService: DevicesService) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      console.log(params);
-      this.device = params.details;
+      this.getDevice(params.details);
     });
   }
 
+  getDevice(deviceId) {
+    this.devicesService.getById(deviceId).subscribe(item => {
+      this.device = item;
+    });
+  }
 }
